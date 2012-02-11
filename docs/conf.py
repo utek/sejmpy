39d48f25bd1b257d234d0c42b40520fc258c143c
utek <mail@utek.pl>
2012-02-12 00:37:49 +0100

    poprawki w dokumentacji

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,8 @@ sys.path.insert(0, os.path.abspath('../.'))
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ["sphinx.ext.intersphinx",
               "sphinx.ext.autodoc",
-              "sphinx.ext.todo"]
+              "sphinx.ext.todo",
+              'sphinx.ext.viewcode']
 
 
 intersphinx_mapping = {'python': ('http://docs.python.org/2.7', None)}
